9c4a87736c0608e8ef9c0fac78ac33697d52c3bf
Brian Woodward <brian.woodward@gmail.com>
2014-07-25 12:03:10 -0400

    initial setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * grade <https://github.com/doowb/grade>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
